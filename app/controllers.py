2f64ee1353859ae951e41e79ce8a3863e0e93008
romamyronenko <eoma575@gmail.com>
2020-07-11 01:17:35 +0300

    Added get methods in rest

--- app/controllers.py
+++ app/controllers.py
@@ -34,18 +34,39 @@ def post_wrapper(f):
         val = validate(p_args, names)
         if val[0]:
             return val
+
         db.session.add(table(*val[1]))
         db.session.commit()
-
         return p_args['name'], 201
-
     return wrapper
 
 
+def get_wrapper(many=True):
+    """
+    Wrapper for get method
+    if many = False return one value by condition
+    else return all values
+    """
+    def func(f):
+        def wrapper(*args, **kwargs):
+            if not many:
+                table, condition = f(*args, **kwargs)
+                value = table.query.filter_by(**condition).first()
+                # value.__dict__[name] - return value of column `name`
+                return {i: value.__dict__[i] for i in table.__table__.columns.keys()}
+
+            table = f(*args, **kwargs)
+            values = table.query.all()
+            # table.__table__.columns.keys() - return list of column names
+            return [{i: value.__dict__[i] for i in table.__table__.columns.keys()} for value in values]
+        return wrapper
+    return func
+
+
 class RoleApi(Resource):
+    @get_wrapper(many=False)
     def get(self, name):
-        role = models.Role.query.filter_by(name=name).first()
-        return role.name
+        return models.Role, {'name': name}
 
     def put(self, name):
         pass
@@ -60,17 +81,15 @@ class RoleListApi(Resource):
     def post(self):
         return ('name',), models.Role
 
+    @get_wrapper()
     def get(self):
-        roles = models.Role.query.all()
-        return [role.name for role in roles]
+        return models.Role
 
 
 class UserApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        user = models.User.query.filter_by(id=id).first()
-        return {user.id: {'username': user.username,
-                          'name': user.name,
-                          'role': user.role}}
+        return models.User, {'id': id}
 
     def put(self, id):
         user = models.User.query.filter_by(id=id).first()
@@ -98,18 +117,16 @@ class UserListApi(Resource):
         """Add new User"""
         return ('username', 'name', 'password', 'role'), models.User
 
+    @get_wrapper()
     def get(self):
         """Return a list of Users"""
-        users = models.User.query.all()
-        return {user.id: {'username': user.username,
-                          'name': user.name,
-                          'role': user.role} for user in users}
+        return models.User
 
 
 class PublicationStatusApi(Resource):
+    @get_wrapper(many=False)
     def get(self, name):
-        publication_status = models.PublicationStatus.query.filter_by(name=name).first()
-        return publication_status.name
+        return models.PublicationStatus, {'name': name}
 
     def put(self, name):
         pass
@@ -124,16 +141,15 @@ class PublicationStatusListApi(Resource):
     def post(self):
         return ('name',), models.PublicationStatus
 
+    @get_wrapper()
     def get(self):
-        pubs = models.PublicationStatus.query.all()
-        return [pub.name for pub in pubs]
+        return models.PublicationStatus
 
 
 class GroupApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        group = models.Group.query.filter_by(id=id).first()
-        return {group.id: {'name': group.name,
-                           'teacher_id': group.teacher_id}}
+        return models.Group, {'id': id}
 
     def put(self, id):
         group = models.Group.query.filter_by(id=id).first()
@@ -154,20 +170,15 @@ class GroupListApi(Resource):
     def post(self):
         return ('name', 'teacher_id'), models.Group
 
+    @get_wrapper()
     def get(self):
-        groups = models.Group.query.all()
-        return {group.id: {'name': group.name,
-                           'teacher_id': group.teacher_id} for group in groups}
+        return models.Group
 
 
 class PublicationApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        publication = models.Publication.query.filter_by(id=id).first()
-        return {publication.id: {'teacher_id': publication.teacher_id,
-                                 'name': publication.name,
-                                 'status': publication.status,
-                                 'content': publication.content,
-                                 'date': str(publication.date)}}
+        return models.Publication, {'id': id}
 
     def put(self, id):
         publication = models.Publication.query.filter_by(id=id).first()
@@ -195,20 +206,15 @@ class PublicationListApi(Resource):
     def post(self):
         return ('teacher_id', 'name', 'status', 'content'), models.Publication
 
+    @get_wrapper()
     def get(self):
-        publications = models.Publication.query.all()
-        return {publication.id: {'teacher_id': publication.teacher_id,
-                                 'name': publication.name,
-                                 'status': publication.status,
-                                 'content': publication.content,
-                                 'date': str(publication.date)} for publication in publications}
+        return models.Publication
 
 
 class GroupStudentApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        gr_st = models.GroupStudent.query.filter_by(id=id).first()
-        return {gr_st.id: {'student_id': gr_st.student_id,
-                           'group_id': gr_st.group_id}}
+        return models.GroupStudent, {'id': id}
 
     def put(self, id):
         gr_st = models.GroupStudent.query.filter_by(id=id).first()
@@ -226,15 +232,15 @@ class GroupStudentListApi(Resource):
     def post(self):
         return ('group_id', 'student_id'), models.GroupStudent
 
+    @get_wrapper()
     def get(self):
-        gr_sts = models.GroupStudent.query.all()
-        return {gr_st.id: {'student_id': gr_st.student_id,
-                           'group_id': gr_st.group_id} for gr_st in gr_sts}
+        return models.GroupStudent
 
 
 class PublicationPermissionStudentApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        pass
+        return models.PublicationPermissionStudent, {'id': id}
 
     def put(self, id):
         pass
@@ -249,13 +255,15 @@ class PublicationPermissionStudentListApi(Resource):
     def post(self):
         return ('publication_id', 'student_id'), models.PublicationPermissionStudent
 
+    @get_wrapper()
     def get(self):
-        pass
+        return models.PublicationPermissionStudent
 
 
 class PublicationPermissionGroupApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        pass
+        return models.PublicationPermissionGroup, {'id': id}
 
     def put(self, id):
         pass
@@ -270,13 +278,15 @@ class PublicationPermissionGroupListApi(Resource):
     def post(self):
         return ('publication_id', 'group_id'), models.PublicationPermissionGroup
 
+    @get_wrapper()
     def get(self):
-        pass
+        return models.PublicationPermissionGroup
 
 
 class TaskApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        pass
+        return models.Task, {'id': id}
 
     def put(self, id):
         pass
@@ -291,13 +301,15 @@ class TaskListApi(Resource):
     def post(self):
         return ('teacher_id',), models.Task
 
+    @get_wrapper()
     def get(self):
-        pass
+        return models.Task
 
 
 class RatingFieldsApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        pass
+        return models.RatingFields, {'id': id}
 
     def put(self, id):
         pass
@@ -312,13 +324,15 @@ class RatingFieldsListApi(Resource):
     def post(self):
         return ('task_id', 'group_id', 'field_name', 'required'), models.RatingFields
 
+    @get_wrapper()
     def get(self):
-        pass
+        return models.RatingFields
 
 
 class RatingListApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        pass
+        return models.RatingList, {'id': id}
 
     def put(self, id):
         pass
@@ -333,15 +347,17 @@ class RatingListListApi(Resource):
     def post(self):
         return ('student_id', 'field_id', 'mark'), models.RatingList
 
+    @get_wrapper()
     def get(self):
-        pass
+        return models.RatingList
 
 
 class QuestionTypeApi(Resource):
-    def get(self, id):
-        pass
+    @get_wrapper(many=False)
+    def get(self, name):
+        return models.QuestionType, {'name': name}
 
-    def put(self, id):
+    def put(self, name):
         pass
 
     @delete_wrapper
@@ -354,13 +370,15 @@ class QuestionTypeListApi(Resource):
     def post(self):
         return ('name',), models.QuestionType
 
+    @get_wrapper()
     def get(self):
-        pass
+        return models.QuestionType
 
 
 class QuestionApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        pass
+        return models.Question, {'id': id}
 
     def put(self, id):
         pass
@@ -375,13 +393,15 @@ class QuestionListApi(Resource):
     def post(self):
         return ('task_id', 'number', 'question', 'correct_answer', 'type', 'max_mark'), models.Question
 
+    @get_wrapper()
     def get(self):
-        pass
+        return models.Question
 
 
 class TestsApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        pass
+        return models.Tests, {'id': id}
 
     def put(self, id):
         pass
@@ -396,13 +416,15 @@ class TestsListApi(Resource):
     def post(self):
         return ('question_id', 'answer'), models.Tests
 
+    @get_wrapper()
     def get(self):
-        pass
+        return models.Tests
 
 
 class AnswersApi(Resource):
+    @get_wrapper(many=False)
     def get(self, id):
-        pass
+        return models.Answers, {'id': id}
 
     def put(self, id):
         pass
@@ -417,15 +439,38 @@ class AnswersListApi(Resource):
     def post(self):
         return ('question_id', 'answer', 'student_id'), models.Answers
 
+    @get_wrapper()
     def get(self):
-        pass
+        return models.Answers
 
 
-api.add_resource(RoleListApi, '/rest/roles')
 api.add_resource(RoleApi, '/rest/roles/<name>')
-api.add_resource(UserListApi, '/rest/users')
+api.add_resource(RoleListApi, '/rest/roles')
 api.add_resource(UserApi, '/rest/users/<id>')
-api.add_resource(GroupListApi, '/rest/groups')
+api.add_resource(UserListApi, '/rest/users')
+api.add_resource(PublicationStatusApi, '/rest/publications_status/<name>')
+api.add_resource(PublicationStatusListApi, '/rest/publications_status')
 api.add_resource(GroupApi, '/rest/groups/<id>')
-api.add_resource(PublicationListApi, '/rest/publications')
+api.add_resource(GroupListApi, '/rest/groups')
 api.add_resource(PublicationApi, '/rest/publications/<id>')
+api.add_resource(PublicationListApi, '/rest/publications')
+api.add_resource(GroupStudentApi, '/rest/group_student/<id>')
+api.add_resource(GroupStudentListApi, '/rest/group_student')
+api.add_resource(PublicationPermissionStudentApi, '/rest/publications_permissions_students/<id>')
+api.add_resource(PublicationPermissionStudentListApi, '/rest/publications_permissions_students')
+api.add_resource(PublicationPermissionGroupApi, '/rest/publications_permissions_groups/<id>')
+api.add_resource(PublicationPermissionGroupListApi, '/rest/publications_permissions_groups')
+api.add_resource(TaskApi, '/rest/tasks/<id>')
+api.add_resource(TaskListApi, '/rest/tasks')
+api.add_resource(RatingFieldsApi, '/rest/rating_fields/<id>')
+api.add_resource(RatingFieldsListApi, '/rest/rating_fields')
+api.add_resource(RatingListApi, '/rest/rating_lists/<id>')
+api.add_resource(RatingListListApi, '/rest/rating_lists')
+api.add_resource(QuestionTypeApi, '/rest/question_types/<name>')
+api.add_resource(QuestionTypeListApi, '/rest/question_types')
+api.add_resource(QuestionApi, '/rest/questions/<id>')
+api.add_resource(QuestionListApi, '/rest/questions')
+api.add_resource(TestsApi, '/rest/tests/<id>')
+api.add_resource(TestsListApi, '/rest/tests')
+api.add_resource(AnswersApi, '/rest/answers/<id>')
+api.add_resource(AnswersListApi, '/rest/answers')
